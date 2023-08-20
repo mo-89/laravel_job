3381ffa58f8e4c602944d80df8b27195e760a3a5
mo <mo@monoMacBook-Air.local>
2023-08-20 20:47:49 +0900

    ログ設定の追加、日時で出力されている行の取得

--- app/Console/Commands/RecoveryWebhook.php
+++ app/Console/Commands/RecoveryWebhook.php
@@ -2,7 +2,9 @@
 
 namespace App\Console\Commands;
 
+use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 
 class RecoveryWebhook extends Command
 {
@@ -25,6 +27,45 @@ class RecoveryWebhook extends Command
      */
     public function handle()
     {
-        //
+        Log::channel('recoveryWebhook')->info('start recovery webhook script');
+
+        //ログファイルを選択
+        $dir = '';
+        $now = new Carbon();
+        $today = $now->format('Y-m-d');
+        $laravelLogFilePath = storage_path("logs/laravel-{$today}.log");
+        if (!file_exists($laravelLogFilePath)) {
+            Log::channel('recoveryWebhook')->info('file not exist');
+        }
+
+        Log::channel('recoveryWebhook')->info($laravelLogFilePath);
+
+        // 読み込んで、POSTデータの取得
+
+        // 直近2時間分のログを取得と仮定
+        // 探索範囲は定数にする
+        $searchRangeTimestamp = Carbon::now()->subHours(2)->timestamp;
+
+        $pattern = '/\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})\]/';
+        $handle = fopen($laravelLogFilePath, 'r');
+        if ($handle) {
+            while(($line = fgets($handle)) !== false) {
+                if (preg_match($pattern, $line, $matches)) {
+                    $logTimestamp = Carbon::parse($matches[1])->timestamp;
+                    if ($logTimestamp >= $searchRangeTimestamp) {
+
+                        //todo エラーの場合データを取得
+                        Log::channel('recoveryWebhook')->info($line);
+                    }
+                }
+            }
+            fclose($handle);
+        } else {
+
+        }
+
+        // job再実行
+
+        Log::channel('recoveryWebhook')->info('finished recovery webhook script');
     }
 }
--- config/logging.php
+++ config/logging.php
@@ -126,6 +126,11 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+
+        'recoveryWebhook' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/recovery-webhook.log'),
+        ]
     ],
 
 ];
